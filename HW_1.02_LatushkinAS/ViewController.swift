d2f090a0181c5b3774252a349e873465667034fa
AssanCred <temix@live.ru>
2023-01-31 15:14:03 +0700

    Initial Commit

---
+++ HW_1.02_LatushkinAS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_1.02_LatushkinAS
+//
+//  Created by Артём Латушкин on 31.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
